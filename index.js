ce054d232a42bb9dc6dbe15ce04107b4d58c9125
Yu <tuba0927hy@gmail.com>
2020-09-07 18:14:38 +0900

    feat: add parameter settings

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 // import module
 const server = require("express")();
 const line = require("@line/bot-sdk");
+
+// parameter settings
+const line_config = {
+    channelAccessToken: process.env.LINE_ACCESS_TOKEN,
+    channelSecret: process.env.LINE_CHANNEL_SECRET
+};
